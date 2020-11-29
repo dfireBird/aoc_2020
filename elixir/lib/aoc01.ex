7c77483870d64ecfccd5e014afeea66c2f6b25b5
dfireBird <me@dfirebird.codes>
2020-11-30 01:20:50 +0530

    chore: add elixir project

---
+++ elixir/lib/aoc01.ex
@@ -0,0 +1,5 @@
+defmodule Aoc01 do
+  def hello_world() do
+    :hello_world
+  end
+end
---
+++ elixir/test/aoc01_test.exs
@@ -0,0 +1,7 @@
+defmodule Aoc1Test do
+  use ExUnit.Case, async: true
+
+  test "part 1" do
+    assert 1 == 1
+  end
+end
