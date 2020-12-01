b7e9507610ba73d358f40ac6ce7cc8c0e94b5b2a
dfireBird <me@dfirebird.codes>
2020-12-01 12:07:06 +0530

    chore: add elixir day01

--- elixir/lib/advent_of_code/day_01.ex
+++ elixir/lib/advent_of_code/day_01.ex
@@ -1,7 +1,20 @@
 defmodule AdventOfCode.Day01 do
-  def part1(args) do
+
+  def arrange(0, _), do: [[]]
+  def arrange(_, []), do: []
+  def arrange(n, [x | xs]) do
+    (for l <- arrange(n - 1, xs), do: [x | l]) ++ arrange(n, xs)
   end
 
-  def part2(args) do
+  def part1(xs) do
+    powerSetOfLength2 = arrange(2, xs)
+    sumTo2020 = Enum.filter(powerSetOfLength2, fn [x,y] -> x + y == 2020 end)
+    hd Enum.map(sumTo2020, fn [x,y] -> x * y end)
+  end
+
+  def part2(xs) do
+    powerSetOfLength3 = arrange(3, xs)
+    sumTo2020 = Enum.filter(powerSetOfLength3, fn [x,y,z] -> x + y + z == 2020 end)
+    hd Enum.map(sumTo2020, fn [x,y,z] -> x * y * z end)
   end
 end
