77d2e77e2e75c1f19f09ea555abbf216fcf8c208
dfireBird <me@dfirebird.codes>
2020-12-02 00:41:53 +0530

    refactor: haskell day01

--- haskell/src/Day01.hs
+++ haskell/src/Day01.hs
@@ -1,26 +1,20 @@
 module Day01 (part1, part2) where
 
-import Data.List (permutations)
-
-pick :: Int -> [a] -> [[a]]
-pick 0 _ = [[]]
-pick _ [] = []
-pick n (x:xs) = map (x:) (pick (n-1) xs) ++ pick n xs
-
 arrange :: Int -> [a] -> [[a]]
-arrange n = concatMap permutations . pick n
+arrange 0 _ = [[]]
+arrange _ [] = []
+arrange n (x : xs) = map (x :) (arrange (n -1) xs) ++ arrange n xs
 
 part1 :: [Int] -> Int
 part1 xs =
-  let
-    powerSetOfLength2 = arrange 2 xs
-    sumTo2020 = filter (\(x:y:_) -> x + y == 2020) powerSetOfLength2
-    result = map (\(x:y:_) -> x * y) sumTo2020
-  in head result
+  let powerSetOfLength2 = arrange 2 xs
+      sumTo2020 = filter (\xs -> sum xs == 2020) powerSetOfLength2
+      result = map product sumTo2020
+   in head result
 
 part2 :: [Int] -> Int
-part2 xs = let
-    powerSetOfLength3 = arrange 3 xs
-    sumTo2020 = filter (\(x:y:z:_) -> x + y + z == 2020) powerSetOfLength3
-    result = map (\(x:y:z:_) -> x * y * z) sumTo2020
-  in head result
+part2 xs =
+  let powerSetOfLength3 = arrange 3 xs
+      sumTo2020 = filter (\xs -> sum xs == 2020) powerSetOfLength3
+      result = map product sumTo2020
+   in head result
