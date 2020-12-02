07f853c16f40d8c9b805a3718c1f2600fb4dc6a4
dfireBird <me@dfirebird.codes>
2020-12-02 12:45:13 +0530

    refactor: day02 haskell

--- haskell/src/Day02.hs
+++ haskell/src/Day02.hs
@@ -1,6 +1,5 @@
 module Day02 where
 
-import Data.List
 import Data.List.Split
 
 type Password = String
@@ -15,24 +14,14 @@ inputDecode (x : xs) =
       [atleast, atmost] = map read . splitOn "-" $ range
    in (Policy atleast atmost (head char), head . words $ password) : inputDecode xs
 
+count :: (a -> Bool) -> [a] -> Int
+count f = length . filter f
+
+xorBool :: Bool -> Bool -> Bool
+xorBool a b = (a && not b) || (not a && b)
+
 part1 :: [(Policy, Password)] -> Int
-part1 [] = 0
-part1 (x : xs) =
-  let Policy atleast atmost char = fst x
-      password = snd x
-      totalNumberOfChar = length . elemIndices char $ password
-   in if totalNumberOfChar <= atmost && totalNumberOfChar >= atleast
-        then 1 + part1 xs
-        else 0 + part1 xs
+part1 = count (\(Policy lo hi c, pass) -> count (== c) pass <= hi && count (== c) pass >= lo)
 
 part2 :: [(Policy, Password)] -> Int
-part2 [] = 0
-part2 (x : xs) =
-  let Policy postion1 position2 char = fst x
-      password = snd x
-   in if password !! (postion1 - 1) == char || password !! (position2 -1) == char
-        then
-          if password !! (postion1 - 1) == char && password !! (position2 -1) == char
-            then 0 + part2 xs
-            else 1 + part2 xs
-        else 0 + part2 xs
+part2 = count (\(Policy p1 p2 c, pass) -> xorBool (pass !! (p1 - 1) == c) (pass !! (p2 -1) == c))
