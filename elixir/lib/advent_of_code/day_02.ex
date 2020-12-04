d9176ad94ea76984ddd05b196167086732696e9a
dfireBird <me@dfirebird.codes>
2020-12-05 00:43:32 +0530

    refactor: day02 solution of elixir

--- elixir/lib/advent_of_code/day_02.ex
+++ elixir/lib/advent_of_code/day_02.ex
@@ -5,7 +5,7 @@ defmodule AdventOfCode.Day02 do
     |> Enum.map(fn [pol, pass] ->
       [r, char] = String.split(pol)
       [lo, hi] = String.split(r, "-") |> Enum.map(&String.to_integer/1)
-      {{:policy, lo, hi, char}, pass}
+      {lo, hi, char, pass}
     end)
   end
 
@@ -19,9 +19,8 @@ defmodule AdventOfCode.Day02 do
 
   def part1(list) do
     count(
-      fn {{:policy, lo, hi, c}, pass} ->
-        count(&(&1 === hd(to_charlist(c))), to_charlist(pass)) <= hi and
-          count(&(&1 === hd(to_charlist(c))), to_charlist(pass)) >= lo
+      fn {lo, hi, c, pass} ->
+        count(&(&1 === hd(to_charlist(c))), to_charlist(pass)) in lo..hi
       end,
       list
     )
@@ -29,7 +28,7 @@ defmodule AdventOfCode.Day02 do
 
   def part2(list) do
     count(
-      fn {{:policy, p1, p2, c}, pass} ->
+      fn {p1, p2, c, pass} ->
         xor_bool(String.at(pass, p1 - 1) == c, String.at(pass, p2 - 1) == c)
       end,
       list
