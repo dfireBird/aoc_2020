568bd70823b4c0e565ee7de73ddceee68ac0fad8
dfireBird <me@dfirebird.codes>
2020-12-14 14:40:01 +0530

    refator: major type refactors in day14 haskell solution

--- haskell/src/Day14.hs
+++ haskell/src/Day14.hs
@@ -1,15 +1,12 @@
 module Day14 where
 
 import Data.Bits
+import Data.List
 import Data.List.Split
 import qualified Data.Map.Strict as Map
 import Data.Word
 
-data OpCode = Mask | Mem Word64 deriving (Show)
-
-data OpVal = MaskV [(Int, Char)] | MemV Integer deriving (Show)
-
-type Instruction = (OpCode, OpVal)
+data Instruction = Mask [(Int, Char)] | Mem Word64 Integer deriving (Show)
 
 part1 :: [Instruction] -> Integer
 part1 inst =
@@ -19,14 +16,12 @@ part1 inst =
     simulate :: [Instruction] -> Map.Map Word64 Integer -> [(Int, Char)] -> Map.Map Word64 Integer
     simulate [] memory _ = memory
     simulate (x : xs) memory mask =
-      let opcode = fst x
-       in case opcode of
-            Mask -> let MaskV opval = snd x in simulate xs memory opval
-            Mem addr ->
-              let MemV opval = snd x
-                  val = foldl maskBit opval mask
-                  newMemory = Map.insert addr val memory
-               in simulate xs newMemory mask
+      case x of
+        Mask opval -> simulate xs memory opval
+        Mem addr opval ->
+          let val = foldl' maskBit opval mask
+              newMemory = Map.insert addr val memory
+           in simulate xs newMemory mask
       where
         maskBit :: Integer -> (Int, Char) -> Integer
         maskBit acc (bN, bitVal) = case bitVal of
@@ -42,14 +37,12 @@ part2 inst =
     simulate :: [Instruction] -> Map.Map Word64 Integer -> [(Int, Char)] -> Map.Map Word64 Integer
     simulate [] memory _ = memory
     simulate (x : xs) memory mask =
-      let opcode = fst x
-       in case opcode of
-            Mask -> let MaskV opval = snd x in simulate xs memory opval
-            Mem addr ->
-              let MemV opval = snd x
-                  val = foldl maskBit [addr] mask
-                  newMemory = foldl (\acc x -> Map.insert x opval acc) memory val
-               in simulate xs newMemory mask
+      case x of
+        Mask opval -> simulate xs memory opval
+        Mem addr opval ->
+          let val = foldl' maskBit [addr] mask
+              newMemory = foldl' (\acc x -> Map.insert x opval acc) memory val
+           in simulate xs newMemory mask
       where
         maskBit :: [Word64] -> (Int, Char) -> [Word64]
         maskBit acc (bN, bitVal) = case bitVal of
@@ -66,7 +59,7 @@ parseInput = map parseLine . lines
        in case take 4 opcode of
             "mask" ->
               let val' = zipWith (\x y -> (35 - x, y)) [0 ..] val
-               in (Mask, MaskV val')
+               in Mask val'
             "mem[" ->
               let memVal = read . head . splitOn "]" . drop 4 $ opcode
-               in (Mem memVal, MemV (read val))
+               in Mem memVal (read val)
